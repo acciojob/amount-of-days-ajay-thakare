b8fc8fd83c76943792a0cbd899b0689067ecdf18
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2025-10-28 17:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+
+function daysOfAYear(year) {
+
+	const leapYear = 366;
+	const nonLeapYear = 365;
+	
+
+	if((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)){
+		return leapYear;
+	}else return nonLeapYear;
+	
+}
+
+
+ const input = prompt("Enter a year:");
+
+  const year = parseInt(input);
+
+  if (isNaN(year)) {
+    alert("❌ Please enter a valid year!");
+  } else {
+    const days = daysOfAYear(year);
+    alert(`✅ The year ${year} has ${days} days.`);
+  }
